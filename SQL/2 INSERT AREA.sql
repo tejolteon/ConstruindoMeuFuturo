a68deec04d548c099cfdc094ab71a2b963869ca6
Guilherme Gonçalves Santos <guilherme.go.sa@gmail.com>
2017-10-30 12:19:35 -0200

    V 0.1.1
    Sricpt banco de dados atualizado para ter menos areas

--- SQL/2 INSERT AREA.sql
+++ SQL/2 INSERT AREA.sql
@@ -1,20 +1,17 @@
 SET IDENTITY_INSERT TB_AREA ON;
 GO
 INSERT INTO TB_AREA(Id_Area,Nome_Area) VALUES
-(1,'Administração'),
-(2,'Negócios e Serviços'),
-(3,'Artes e Design'),
-(4,'Ciências Biológicas'),
-(5,'Ciências Exatas'),
-(6,'Ciências da Terra'),
-(7,'Informática'),
-(8,'Ciências Sociais'),
-(9,'Humanas'),
-(10,'Comunicação e Informação'),
-(11,'Engenharia e Produção'),
-(12,'Ciências da Saúde'),
-(13,'Ciências Agrárias'),
-(14,'Linguística');
+(1,'Administração, Negócios e Serviços'),
+(2,'Artes e Design'),
+(3,'Ciências Biológicas e da terra'),
+(4,'Ciências Exatas'),
+(5,'Informática'),
+(6,'Humanas e Ciências Sociais'),
+(7,'Comunicação e Informação'),
+(8,'Engenharia e Produção'),
+(9,'Ciências da Saúde'),
+(10,'Ciências Agrárias'),
+(11,'Linguística');
 GO
 
 SET IDENTITY_INSERT TB_AREA OFF;

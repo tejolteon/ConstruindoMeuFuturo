e1ca94e7ba33da186114d37c87082f27d3be65ca
Guilherme Gonçalves Santos <guilherme.go.sa@gmail.com>
2017-11-29 10:40:35 -0200

    v1.5.2
    update no sql

---
+++ SQL/10 INSERT QUESTAO.sql
@@ -0,0 +1,19 @@
+SET IDENTITY_INSERT TB_QUESTAO ON;
+GO
+INSERT INTO TB_QUESTAO(Id_Questao,Texto_Questao) VALUES
+(1,'O que você considera mais importante na hora de escolher uma profissão?'),
+(2,'A pessoa da sua família que você mais admira:'),
+(3,'Se você tivesse que montar uma empresa, esta empresa seria como?'),
+(4,'Qual esporte você prefere?'),
+(5,'Em um ambiente onde você não conhece ninguem você prefere:'),
+(6,'Qual dessas personalidades você mais admira?'),
+(7,'Em minhas amizades busco pessoas que:'),
+(8,'Na escola eu geralmente me destacava por:'),
+(9,'Se eu tivesse que escolher uma atividade para exercer por 6 meses seria:'),
+(10,'Eu certamente gastaria uma boa parte do meu primeiro salário com:'),
+(11,'Se eu fosse abrir um destes negócios seria:'),
+(12,'Minha maior frustração seria:');
+GO
+
+SET IDENTITY_INSERT TB_QUESTAO OFF;
+GO
